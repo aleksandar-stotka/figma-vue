2b038dbc03c550e8f12c714c96fdef5d80629874
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-10-11 01:48:31 -0700

    work-on-header-nav

---
+++ data.js
@@ -0,0 +1,10 @@
+export const navLinks = [
+    { text: 'Work', to: '/' },
+    { text: 'What to do', to: '/what to do' },
+    { text: 'Stories', to: '/stories' },
+    { text: 'Contact', to: '/contact' },
+    { text: 'Careers', to: '/careers' },
+    { text: 'About us', to: '/about us' },
+  ];
+
+  
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,62 @@
 <template>
-    <div class="w-full flex items-center justify-center">
-        <header  >
-            <nav class=" p-4">
-  <div class="container mx-auto flex justify-between items-center">
-    <!-- Logo on the left -->
-    <div class="flex items-center space-x-4">
-        <img src="~/assets/images/Group 4.png" alt="Logo" class="h-119 w-150">
-      <img src="~/assets/images/Rectangle 1.png" alt="Logo" class="h-119 w-150">
-      
-    </div>
-    
-    <!-- Links on the right -->
-    <div class="space-x-6 p-5 ">
-      
-      <a href="#" class=" text-linkColors hover:bg-green-500 p-5  w-78 h-41">Work</a>
-      <a href="#" class=" text-linkColors  w-132 h-41 ">What we do</a>
-      <a href="#" class=" text-linkColors  w-78 h-41">Stories</a>
-      <a href="#" class=" text-linkColors  w-78 h-41">Contact</a>
-      <a href="#" class=" text-linkColors  w-78 h-41">Careers</a>
-      <a href="#" class=" text-linkColors  w-78 h-41">About us</a>
-    </div>
-  </div>
-</nav>
-     
->
-        </header>
-      
-    </div>
+  <div class="w-full flex items-center justify-center">
+    <header>
+      <nav class="p-4 w-1246">
+        <div class="container mx-auto flex justify-between items-center">
+          <!-- Logo on the left -->
+          <div class="flex items-center space-x-4">
+            <img src="@/assets/images/logo2.png" alt="Logo" />
+            <img src="@/assets/images/logo.png" alt="img" class="h-119 w-150" />
+          </div>
 
-   
-    <div >
-        <slot/>
-    </div>
-    <footer>
-        footer
-    </footer>
+          <!-- Links on the right -->
+          <div class="basis-3/6 flex space-x-5">
+            <div v-for="(link, index) in linksSource" :key="index">
+              <a
+                class="text-linkColors w-106 h-41 leading-relaxed"
+                :href="link"
+                target="_blank"
+                >{{ link.text }}</a
+              >
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      >
+    </header>
+  </div>
+
+  <div>
+    <slot />
+  </div>
+  <footer>footer</footer>
 </template>
 
 <script>
-export default {
-  setup() {
+import { navLinks, imageLinks } from "~/data";
 
-  }
+export default {
+  data() {
+    return {
+      linksSource: navLinks,
+
+      imageSources: imageLinks,
+    };
+  },
 };
 </script>
 
-<style  scoped>
-  header {
-    background-image: url('~/assets/images/Rectangle 12.png');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 1426px ;
-    height:919px ;
-  
-  }
-  .active-link {
+<style scoped>
+header {
+  background-image: url("~/assets/images/Rectangle 12.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 1426px;
+  height: 919px;
+}
+.active-link {
   color: red; /* Change the color for active links */
 }
-
 </style>
